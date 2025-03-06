66bd1b0f21b2c6905b50d6b904de18e453b8f9e9
student <student@DESKTOP-I3OGHQQ>
2025-03-06 18:08:05 +0100

    Dodaj pliki projektów.

---
+++ lab_0/FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_0
+{
+    internal class FizzBuzz
+    {
+        public int limit; 
+        public FizzBuzz(int limit) {  this.limit = limit; }
+
+        public void solution()
+        {
+            for (int i = 1; i <= limit; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");        
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 != 0 && i % 5 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
---
+++ lab_0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lab_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            FizzBuzz a = new FizzBuzz (20);
+            a.solution();
+        }
+    }
+}
