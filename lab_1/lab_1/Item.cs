be2d46046670287692c0826d94c2fb621cee812a
M1K45 <272570@student.pwr.edu.pl>
2025-03-19 19:01:29 +0100

    First laboratory task after first class

---
+++ lab_1/lab_1/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    class Item
+    {
+        int id;
+        int weight;
+        int value;
+        int ratio;
+
+        public Item(int weight, int value, int id)
+        {
+            this.weight = weight;
+            this.value = value;
+            this.id = id;
+            this.ratio = value/weight;
+        }
+
+        public int getWeight() { return weight; }
+        public int getValue() { return value; }
+
+        
+
+
+    }
+}
---
+++ lab_1/lab_1/Problem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace lab_1
+{
+    class Problem
+    {
+        int numItems;
+        List <Item> items;
+
+        int minValue = 1;
+        int maxValue = 10;
+        
+        public Problem (int n, int seed)
+        {
+            items = new List <Item> ();
+            this.numItems = n;
+            Random random = new Random(seed);
+
+            for (int i = 0; i < n; i++)
+            {
+                Item a = new Item(random.Next(maxValue), random.Next(maxValue), i);
+                items.Add(a);
+
+            }      
+        }
+
+        public override string ToString()
+        {
+            string text = string.Empty;
+           foreach (Item a in items)
+            {
+                text += ("Weight: " + a.getWeight().ToString() + " Value: " + a.getValue().ToString() + "\n");
+            } 
+           return text;
+        }
+
+       
+        Result Solve() { 
+            Result result = new Result();
+            items.Sort();
+            
+            return result;
+
+        }
+
+        
+
+
+    }
+}
